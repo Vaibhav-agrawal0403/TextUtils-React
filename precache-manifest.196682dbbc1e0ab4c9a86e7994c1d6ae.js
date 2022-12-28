ca1910245ad9ae70bdb6d477403a91b4aa30cd77
Vaibhav-agrawal0403 <vaibhavagrawal0403@gmail.com>
2022-12-29 01:27:09 +0530

    Updates

---
+++ precache-manifest.196682dbbc1e0ab4c9a86e7994c1d6ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ed1bc82249bbf32fc4a",
+    "url": "/TextUtils-React/static/css/main.d9d1b54a.chunk.css"
+  },
+  {
+    "revision": "1ed1bc82249bbf32fc4a",
+    "url": "/TextUtils-React/static/js/main.1ed1bc82.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "2591e466b5f270c0c477",
+    "url": "/TextUtils-React/static/js/runtime~main.2591e466.js"
+  },
+  {
+    "revision": "3c76427c13841ab92ffeac986f31003b",
+    "url": "/TextUtils-React/index.html"
+  }
+];
